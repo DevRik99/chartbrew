087fcb1287cbe708e8482e6c1304c4648dc59123
devrik99 <devrikoficial@gmail.com>
2024-07-10 21:08:49 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,4 @@ export VITE_APP_CLIENT_PORT=${VITE_APP_CLIENT_PORT}
 export VITE_APP_ONE_ACCOUNT_EXTERNAL_ID=${VITE_APP_ONE_ACCOUNT_EXTERNAL_ID}
 
 cd server
-NODE_ENV=production nohup node index.js &
-
-# Serve the UI
-npm run start
+NODE_ENV=production nohup node index.js 
