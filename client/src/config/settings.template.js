0c0c3a6024fde2c1032d939e981f7af25f2dfd39
devrik99 <devrikoficial@gmail.com>
2024-07-10 20:29:07 -0500

    feat: Update API and documentation hosts
    The API_HOST and DOCUMENTATION_HOST constants have been updated to use hardcoded values instead of environment variables. This change ensures that the correct hosts are used in both production and development environments.

--- client/src/config/settings.template.js
+++ client/src/config/settings.template.js
@@ -1,12 +1,10 @@
-export const API_HOST = import.meta.env.PROD
-  ? import.meta.env.VITE_APP_API_HOST
-  : (import.meta.env.VITE_APP_API_HOST_DEV || "http://localhost:4019");
+export const API_HOST = "https://chartbrew-1-cx36.onrender.com";
 
 export const SITE_HOST = import.meta.env.PROD
   ? import.meta.env.VITE_APP_CLIENT_HOST
   : (import.meta.env.VITE_APP_CLIENT_HOST_DEV || "http://localhost:4018");
 
-export const DOCUMENTATION_HOST = import.meta.env.PROD ? "https://docs.chartbrew.com" : "http://localhost:8080";
+export const DOCUMENTATION_HOST = "https://docs.chartbrew.com";
 
 export const APP_VERSION = import.meta.env.VITE_APP_VERSION;
 export const ONE_ACCOUNT_ENABLED = !!import.meta.env.VITE_APP_ONE_ACCOUNT_EXTERNAL_ID;
